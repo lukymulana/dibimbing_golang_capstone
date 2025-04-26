cd199c5f10a7f8c9285dc18048dcb7ba7884d4e9
Luky Mulana <lukymulana@gmail.com>
2025-04-26 07:00:27 +0700

    update config db and fix entity migrate

--- config/config.go
+++ config/config.go
@@ -1,21 +1,32 @@
 package config
 
 import (
-	// "fmt"
 	"log"
-
+	"dibimbing_golang_capstone/entity"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func InitDB() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/travel_booking?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func InitDB() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/travel_booking?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
-	log.Println("Connected to database")
+
+	// Auto migrate entities
+	err = db.AutoMigrate(
+		&entity.User{},
+		&entity.Trip{},
+		&entity.Booking{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
+	log.Println("Database migrated successfully")
+	DB = db
+	return db
 }
